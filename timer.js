057d16a6f5702aa2529ff6cb4602debb66549fde
Sarthak Pranesh <sarthakpranesh08@gmail.com>
2019-02-13 13:18:54 +0530

    added a timer

--- timer.js
+++ timer.js
@@ -1,6 +1,5 @@
 var x;
-function timerfuc(){
-var countDownDate = new Date("Dec 17, 2018 15:37:25").getTime();
+var countDownDate = new Date("Feb 23, 2019 15:37:25").getTime();
 
 // Update the count down every 1 second
  x = setInterval(function() {
@@ -32,37 +31,3 @@ var countDownDate = new Date("Dec 17, 2018 15:37:25").getTime();
   }
 
 }, 1000);
-}
-
-//object on click-- checking if the object has been clicked
-b1.onclick = timerfuc();
-
-
-//defining a reset time function
-function resettime(){
-  document.getElementById("day").innerHTML = "0";
-  document.getElementById("hour").innerHTML = "0";
-  document.getElementById("min").innerHTML = "0";
-  document.getElementById("sec").innerHTML = "0";
-  clearInterval(x);
-  alert("Timer cleared");
-}
-
-
-//reseting the timer
-b2.onclick = resettime();
-
-
-//The change of bgcolor of the html on an interwal of 10s
-var i = 0;
-var y = setInterval(function(){
-  var l = ['yellow','white','aqua','pink','red','maroon','tomato','dodgerblue','mediumseagreen','violet'];
-  while(i<=l.length-1){
-  document.body.style.backgroundColor = l[i];
-  i = i + 1;
-  break;
-}
-  if(i==l.length){
-    i = 0;
-  }
-}, 10000);
